9cb91ce5c3f6e73249cc8861825caf6d9b2c2870
PerryKum <2273157479@qq.com>
2024-04-09 14:38:50 +0800

    20240409143850

--- src/components/Details.vue
+++ src/components/Details.vue
@@ -1,18 +1,27 @@
 <template>
-    <div class="details">
+    <div class="details" :style="{ height: detailsBox.height + 'px' }">
         <RouterView />
     </div>
 </template>
 
 <script lang="ts" setup>
-    import { RouterView } from 'vue-router'
+import { onMounted, reactive } from 'vue';
+import { RouterView } from 'vue-router'
+
+const detailsBox = reactive({
+    height: window.innerHeight - 155
+})
+
+window.addEventListener('resize', () => {
+    detailsBox.height = window.innerHeight - 155
+})
 </script>
 
 <style scoped>
-    .details {
-        width: 100%;
-        height: 800px;
-        background: rgb(250, 250, 250);
-        border-radius: 10px;
-    }
+.details {
+    width: 100%;
+    height: 800px;
+    background: rgb(250, 250, 250);
+    border-radius: 10px;
+}
 </style>
