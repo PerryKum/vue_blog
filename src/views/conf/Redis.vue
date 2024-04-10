98ff84f0d82fecf50a7354ee8cd9ca58a2ea4479
PerryKum <2273157479@qq.com>
2024-04-10 19:51:25 +0800

    20240410195125

--- src/views/conf/Redis.vue
+++ src/views/conf/Redis.vue
@@ -2,7 +2,7 @@
     <div class="rtcl">
         <h1>Redis</h1>
         <h3>版本选择：「>=3」</h3>
-        <h3>「<a href="https://redis.io/" target="_blank">官网下载</a>」并「<a href="https://www.perrykum.fun/rtcls/exp/evconf/evconf.html" target="_blank">配置环境变量</a>」。</h3>
+        <h3>「<a href="https://github.com/tporadowski/redis/releases" target="_blank">下载地址</a>」并「<a href="https://www.perrykum.fun/rtcls/exp/evconf/evconf.html" target="_blank">配置环境变量</a>」。</h3>
         <h3>可视化工具推荐：「 <a href="https://github.com/RedisInsight/RedisDesktopManager/" target="_blank">Redis Desktop Manager</a> 」。</h3>
         <h3>「Win + R」输入「cmd」并启动，运行「 <code>Redis-server -v</code> 」查询版本，显示版本号即安装成功。</h3>
     </div>
