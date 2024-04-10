0d91dd30ae935ca415a1d367a5cac76f05aedb5e
PerryKum <2273157479@qq.com>
2024-04-10 15:00:30 +0800

    20240410150030

--- src/views/conf/Vue.vue
+++ src/views/conf/Vue.vue
@@ -3,6 +3,9 @@
         <h1>Vue3</h1>
         <h3>这个没啥好配置的，先写个页面占位。</h3>
         <h3>技术栈：Vue3，Node.js，Typescript/Javascript，ElementPlus。</h3>
+        <h3>安装依赖：在「 <code>RuoYi-Vue-v3.8.7\ruoyi-ui</code> 」下呼出命令行，运行「 <code>npm i</code> 」即可。</h3>
+        <h3 style="color: red;">【注意】（随时补充）</h3>
+        <h3>1、「package.json」中第 64 行，「 @vue/cli-plugin-babel 」版本过新（ >=5 ）会出问题，实测「 4.4.6 」版本可用。若已安装该依赖，则删除「 <code>RuoYi-Vue-v3.8.7\ruoyi-ui\node_modules\@babel</code> 」、将该键的值改为「 4.4.6 」，再重新安装依赖即可。</h3>
     </div>
 </template>
 
